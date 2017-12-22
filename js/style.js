3794d1c5a88b92d1a5d17624b61dfca430eac507
riyawalia <riyawalia12@gmail.com>
2017-12-22 17:11:06 +0530

    Added moving stars to home background

---
+++ js/style.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    color: '#C7C7CE',
+    connectParticles: false,
+    maxParticles: 350,
+    sizeVariations: 3.5,
+  });
+};
