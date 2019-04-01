b364c3f82e7e3d8def7cd94c685eaabcec350fc2
riyawalia <riyawalia12@gmail.com>
2019-03-31 22:43:45 -0400

    changign star density

--- js/style.js
+++ js/style.js
@@ -2,8 +2,10 @@ window.onload = function() {
   Particles.init({
     selector: '.background',
     color: '#C7C7CE',
+    //color: '#BC8F8F',
+    //color: '#FFB6C1',
     connectParticles: false,
-    maxParticles: 350,
-    sizeVariations: 3.5,
+    maxParticles: 500,
+    sizeVariations: 2.5,
   });
 };
